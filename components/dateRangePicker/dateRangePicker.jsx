75078126cf91669721a5913200d5f17b1ff450a3
Carlos Marten <martencarlos@gmail.com>
2023-06-27 00:46:45 +0200

    datepicker optimized for mobile

--- components/dateRangePicker/dateRangePicker.jsx
+++ components/dateRangePicker/dateRangePicker.jsx
@@ -4,11 +4,14 @@ import { useRef } from "react"
 import DatePicker from "react-multi-date-picker"
 import "./component.css"
 import styles from "./dateRangePicker.module.css"
+import "react-multi-date-picker/styles/layouts/mobile.css"
 import { BsFillCalendarWeekFill } from "react-icons/bs";
-
+import { useWindowSize } from "@/hooks/windowSize"
 
 function DateRangePicker() {
     const datePickerRef = useRef()
+    const { width } = useWindowSize()
+ 
 
     return (
         <div className={styles.dateRangePicker}>
@@ -18,6 +21,7 @@ function DateRangePicker() {
                 // value={new Date()}
                 placeholder="Arrival & Departure dates"
                 // dateSeparator=" to "
+                className={width<600?"rmdp-mobile":"rmdp-desktop"}
                 range
                 editable = {false}
                 rangeHover
