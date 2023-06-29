a4b63d44dc0b29d21b9dcdaa2cdebffe24e888aa
Carlos Marten <martencarlos@gmail.com>
2023-06-29 15:30:45 +0200

    updated modal

--- components/landing/gallery/gallery.jsx
+++ components/landing/gallery/gallery.jsx
@@ -6,7 +6,24 @@ import { BsXLg } from "react-icons/bs";
 import { BsChevronLeft } from "react-icons/bs";
 import { BsChevronRight } from "react-icons/bs";
 
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
 
+const toBase64 = (str) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 function Gallery() {
 
@@ -55,12 +72,13 @@ function Gallery() {
         <div id="imgModal" className={styles.modal}>
             <div className={styles.galleryContainer}>
                 <Image
-                    src={selectedImageId ? selectedImageSrc : "/maingallery/1.webp"}
-                    alt="Exterior pool view of a villa"
+                    src={selectedImageId ? selectedImageSrc : "/logo.webp"}
+                    alt="gallery"
                     className={styles.modalImage}
-                    width={800}
-                    quality={100}
-                    height={680}
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                    width={700}
+                    height={475}
                 />
                 <div className={styles.closeButton} onClick={closeModal}>
                     <BsXLg />
