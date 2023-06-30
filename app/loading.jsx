a0d042bd391866c52cb87440af02aff7d5535efd
Carlos Marten <martencarlos@gmail.com>
2023-06-30 23:40:59 +0200

    added loading fallbacks

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Loading from '@/components/loading/loading'
+
+function loading() {
+  return (
+    <Loading fallback= "Loading..."/>
+  )
+}
+
+export default loading
