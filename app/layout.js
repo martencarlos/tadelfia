89c40c5d4c3b25f4e8f3e067e0a553d358bd196a
Carlos Marten <martencarlos@gmail.com>
2023-07-03 10:17:10 +0200

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import  Navbar from '@/components/navbar/navbar'
 import styles from './layout.module.css'
 import AuthProvider from "@/components/authProvider/authProvider";
-
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +12,35 @@ export const metadata = {
   description: 'Villas in Tadelfia',
 }
 
+
+
 export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-NLD045G618"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NLD045G618', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+     
       <body className={inter.className}>
         <Navbar/>
         <AuthProvider>
