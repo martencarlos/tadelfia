cd01e8180c1c820d6ae0dec9eca3efcdc1cf49f2
Carlos Marten <martencarlos@gmail.com>
2023-07-07 17:35:42 +0200

    Updated admin bookings page

--- app/(admin)/dashboard/bookings/page.jsx
+++ app/(admin)/dashboard/bookings/page.jsx
@@ -1,84 +1,133 @@
-
-
-import styles from './page.module.css'
+import styles from "./page.module.css";
 // import { serverGetAllBookings } from '@/lib/booking'
-import Link from 'next/link'
-
+import Link from "next/link";
 
 export default async function Bookings() {
-    let allBookings = []
-    try {
-        const res = await fetch(process.env.NEXT_PUBLIC_HOST+"/api/bookings",{
-            next: { revalidate: 10},
-        })
-        allBookings = await res.json()
-     
-    } catch (error) {
-        console.log(error)
-    }
-   
-    return (
+  let allBookings = [];
+  try {
+    const res = await fetch(process.env.NEXT_PUBLIC_HOST + "/api/bookings", {
+      next: { revalidate: 10 },
+    });
+    allBookings = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+
+  return (
     <div className={styles.bookings}>
-        <div className={styles.header}>
-            <h1>Bookings</h1>
-            {allBookings.length === 0 &&<div>
-                No bookings yet
-            </div>}
+      <div className={styles.header}>
+        <h1>Bookings</h1>
+        {allBookings.length === 0 && <div>No bookings yet</div>}
+      </div>
+      <div className={styles.bookingsList}>
+        <div className={styles.bookingHeader}>
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Index</h3>
+            </div>
+          </div>
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Booking Nº</h3>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Apartment</h3>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Check-in/out</h3>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Accomodation</h3>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <h3>Payment</h3>
+            </div>
+          </div>
         </div>
-        <div className={styles.bookingsList}>
-            { allBookings.map((booking) => (
-                <Link href={"/dashboard/bookings/"+booking._id} key={booking._id} className={styles.booking}>
-                    <div className={styles.section}>
-                        <div className={styles.sectionTitle}>
-                            <h3>Contact</h3>
-                        </div>
-                        <div className={styles.sectionContent}>
-                            <div>{booking.contact.firstName}</div>
-                            <div>{booking.contact.lastName}</div>
-                            <div className={styles.elipsis}>{booking.contact.email}</div>
-                        </div>
-                    </div>
+        {allBookings.map((booking, i) => (
+          <Link
+            href={"/dashboard/bookings/" + booking._id}
+            key={booking._id}
+            className={styles.booking}
+          >
+            <div className={styles.section}>
+              
+                <h3>{"#" + i}</h3>
+              
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>
+                <h3>Booking Nº</h3>
+              </div>
+              <div className={styles.sectionContent}>
+                <div className={styles.elipsis}>
+                  {" "}
+                  {"# " + booking.payment.id.slice(-6)}
+                </div>
+              </div>
+            </div>
 
-                    <div className={styles.section}>
-                        <div className={styles.sectionTitle}>
-                            <h3>Address</h3>
-                        </div>
-                        <div className={styles.sectionContent}>
-                            <div className={styles.elipsis}>{booking.address.street}</div>
-                            <div className={styles.elipsis}>{booking.address.postal}</div>
-                            <div>{booking.address.towncity}</div>
-                            <div>{booking.address.country}</div>
-                        </div>
-                    </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>
+                <h3>Apartment</h3>
+              </div>
+              <div className={styles.sectionContent}>
+                <div>{booking.accomodation.villa}</div>
+              </div>
+            </div>
 
-                    <div className={styles.section}>
-                        <div className={styles.sectionTitle}>
-                            <h3>Accomodation</h3>
-                        </div>
-                        <div className={styles.sectionContent}>
-                            <div>{booking.accomodation.villa}</div>
-                            <div>{new Date(booking.accomodation.checkin).toLocaleDateString()}</div>
-                            <div>{new Date (booking.accomodation.checkout).toLocaleDateString()}</div>
-                            <div>{booking.accomodation.nights+ " nights"}</div>
-                            <div>{booking.accomodation.guests+ " guests"}</div>
-                        </div>
-                    </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>
+                <h3>Check-in/out</h3>
+              </div>
+              <div className={styles.sectionContent}>
+                <div>
+                  {new Date(booking.accomodation.checkin).toLocaleDateString()}
+                </div>
+                <div>
+                  {new Date(booking.accomodation.checkout).toLocaleDateString()}
+                </div>
+              </div>
+            </div>
 
-                    <div className={styles.section}>
-                        <div className={styles.sectionTitle}>
-                            <h3>Payment</h3>
-                        </div>
-                        <div className={styles.sectionContent}>
-                            <div className={styles.elipsis}>{booking.payment.id}</div>
-                            <div>{new Date(booking.createdAt).toLocaleDateString()}</div>
-                            <div>{booking.payment.currency+" "+booking.payment.amount}</div>
-                        </div>
-                    </div>
-                </Link>
-            ))}
-        </div>
-                    
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>
+                <h3>Accomodation</h3>
+              </div>
+              <div className={styles.sectionContent}>
+                <div>{booking.accomodation.nights + " nights"}</div>
+                <div>{booking.accomodation.guests + " guests"}</div>
+              </div>
+            </div>
+
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>
+                <h3>Payment</h3>
+              </div>
+              <div className={styles.sectionContent}>
+                <div>{new Date(booking.createdAt).toLocaleDateString()}</div>
+                <div>
+                  {booking.payment.currency === "eur"
+                    ? "€ " + booking.payment.amount
+                    : booking.payment.currency + " " + booking.payment.amount}
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-    )
+  );
 }
-
