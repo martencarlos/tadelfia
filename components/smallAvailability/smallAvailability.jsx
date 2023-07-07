44f5dfcebd696978eb7bae3c9dcb3f8b1e49628e
Carlos Marten <martencarlos@gmail.com>
2023-07-07 15:00:15 +0200

    working small availability

--- components/smallAvailability/smallAvailability.jsx
+++ components/smallAvailability/smallAvailability.jsx
@@ -4,63 +4,40 @@
 import styles from "./smallAvailability.module.css";
 import "./component.css";
 import { useEffect, useState } from "react";
-import { getYearBookingsFromVilla, getAllBookingRanges } from "@/lib/booking";
+import { getAllBookings, getAllBookingRanges } from "@/lib/booking";
 import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
+
 
 function SmallAvailability({ villa }) {
   // const size = useWindowSize();
   const [ranges, setRanges] = useState([]); // Booked ranges
+  
 
   useEffect(() => {
-    const newRanges = [];
+    
     if (villa !== "Villa") {
-      getYearBookingsFromVilla(new Date().getFullYear(), "Villa").then(
-        (data) => {
-          if (data.length > 0) {
-            const bookedRanges = data.map((booking) => {
+
+      getAllBookings().then((data) => {
+        
+        if (data.length > 0) {
+          const bookedRanges = data.map((booking) => {
+            if(booking.accomodation.villa === villa || booking.accomodation.villa === "Villa"){
+              
               const checkin = new Date(booking.accomodation.checkin);
               const checkout = new Date(booking.accomodation.checkout);
               return [
-                new Date(
-                  checkin.getFullYear(),
-                  checkin.getMonth(),
-                  checkin.getDate()
-                ),
-                new Date(
-                  checkout.getFullYear(),
-                  checkout.getMonth(),
-                  checkout.getDate()
-                ),
+                new Date( checkin.getFullYear(), checkin.getMonth(), checkin.getDate()),
+                new Date( checkout.getFullYear(), checkout.getMonth(), checkout.getDate()),
               ];
-            });
-            setRanges(...ranges, bookedRanges);
-            // newRanges.push(...bookedRanges);
-          }
-        }
-      );
-
-      getYearBookingsFromVilla(new Date().getFullYear(), villa).then((data) => {
-        if (data.length > 0) {
-          const bookedRanges = data.map((booking) => {
-            const checkin = new Date(booking.accomodation.checkin);
-            const checkout = new Date(booking.accomodation.checkout);
-            return [
-              new Date(
-                checkin.getFullYear(),
-                checkin.getMonth(),
-                checkin.getDate()
-              ),
-              new Date(
-                checkout.getFullYear(),
-                checkout.getMonth(),
-                checkout.getDate()
-              ),
-            ];
+            }
           });
+          console.log(bookedRanges)
           setRanges(...ranges, bookedRanges);
         }
       });
+
+    
     } else {
       getAllBookingRanges(new Date().getFullYear()).then((data) => {
         if (data.length > 0) {
@@ -86,6 +63,7 @@ function SmallAvailability({ villa }) {
     }
   }, []);
 
+  // console.log(ranges)
   return (
     <div className={styles.availability}>
       {/*} <h1 className={styles.h1}>Availability</h1>*/}
@@ -98,7 +76,7 @@ function SmallAvailability({ villa }) {
               if (
                 ranges.find(
                   (x) =>
-                    x[0].setHours(0, 0, 0, 0) <= date.setHours(0, 0, 0, 0) &&
+                    x !== undefined && x[0].setHours(0, 0, 0, 0) <= date.setHours(0, 0, 0, 0) &&
                     x[1].setHours(0, 0, 0, 0) >= date.setHours(0, 0, 0, 0)
                 )
               ) {
